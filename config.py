81c6a997154f630ead7c4efb93f80059b5be8687
10811735 <mbadassou@st.ug.edu.gh>
2023-02-15 11:32:59 +0000

    deploy

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgres://mawuli:ZeV4bKGI9llkpfqynrsEHqs13exW6YmL@dpg-cfmbj14gqg469ktg1flg-a/db_zz2w"
+DATABASE_URL = "postgresql://mawuli:ZeV4bKGI9llkpfqynrsEHqs13exW6YmL@dpg-cfmbj14gqg469ktg1flg-a/db_zz2w"
 
 engine = create_engine(DATABASE_URL)
 
